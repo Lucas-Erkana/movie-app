f86facb4205ce02367df51543c46bac5198eb3d0
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-14 18:59:33 +0200

    Search function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { useEffect } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+
 //656b3e1
 const API_URL = 'http://www.omdbapi.com?apikey=656b3e1';
 
@@ -6,7 +9,7 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    console.log(data.Searchgit);
   }
 
   useEffect(()=>{
@@ -14,10 +17,25 @@ const App = () => {
   },[]);
 
   return (
-    <div className="App">
-     <h1>App</h1>
+    <div className="app">
+     <h1>MovieHub</h1>
+     <div className='search'>
+      <input 
+      placeholder='Search for movies'
+      value='Search'
+      onChange={()=>{}}
+      />
+      <img
+      src={SearchIcon}
+      alt='search'
+      onClick={() => {}}
+      />
+     </div>
+     <div className='container'>
+      
+     </div>
     </div>
-  );
+  )
 }
 
 export default App;
