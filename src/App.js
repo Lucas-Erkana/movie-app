cc09b5788dae95978d686a6a34ff4d00b56a7fc8
Lucas David Erkana <lderkana21x@gmail.com>
2023-01-14 19:50:05 +0200

    Add mivie object

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
 
-//656b3e1
+// 656b3e1
 const API_URL = 'http://www.omdbapi.com?apikey=656b3e1';
+const movie1 = {
+
+  Title: 'Superman, Spiderman or Batman',
+  Year: '2011',
+  imdbID: 'tt2084949',
+  Type: 'movie',
+  Poster: 'https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg',
+};
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Searchgit);
-  }
+    setMovies(data.Search);
+  };
 
-  useEffect(()=>{
-    searchMovies('Spiderman')
-  },[]);
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
 
   return (
     <div className="app">
-     <h1>MovieHub</h1>
-     <div className='search'>
-      <input 
-      placeholder='Search for movies'
-      value='Search'
-      onChange={()=>{}}
-      />
-      <img
-      src={SearchIcon}
-      alt='search'
-      onClick={() => {}}
-      />
-     </div>
-     <div className='container'>
-      
-     </div>
+      <h1>MovieHub</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Search"
+          onChange={() => {}}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => {}}
+        />
+      </div>
+      <div className="container">
+        <MovieCard movie1={movie1} />
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
